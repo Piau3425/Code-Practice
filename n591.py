cf502d7be91e04244c70429a79fc18dc90cc6a02
Piau <piau7849@gmail.com>
2024-05-10 21:07:51 +0800

    update

--- n591.py
+++ n591.py
@@ -1,8 +1,15 @@
-animal = "屬牛虎兔龍蛇馬羊猴雞狗豬"
-try:
-    while true:
-        year = int(input())
-        ind = (year) % 12
-        print(animal[ind])
-except:
-    pass
+
+
+myList = ['a', 'b', 'c', 'd', 'e']
+# 新增
+myList.append('X')
+print(myList)
+# 插入
+myList.insert(3,'Y')
+print(myList)
+# 刪除某元素
+myList.remove('b')
+print(myList)
+# 刪除某位置
+myList.pop(4)
+print(myList)
