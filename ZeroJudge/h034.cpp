c5a8e8e05c4091d80b78ffecab75e4a919a88361
Piau <piau7849@gmail.com>
2024-06-05 14:26:58 +0800

    240605

--- ZeroJudge/h034.cpp
+++ ZeroJudge/h034.cpp
@@ -1,24 +1,20 @@
-/*TLE*/
-
 #include <iostream>
 #include <vector>
-#include <cmath>
 using namespace std;
 
 int main(){ ios_base::sync_with_stdio(0); cin.tie(0);
-    int n, maxx;
+    int n, maxx = 0;
     cin >> n;
     vector<string> v(n);
     
-    for(int i = 0; i < n; i++)
+    for(int i = 0; i < n; i++){
         cin >> v[i];
-    
-    for(int i = 0; i < n; i++)
         maxx = max(maxx, int(v[i].length()));
+    }
 
     for(int i = 0; i < maxx; i++){
         for(int j = 0; j < n; j++){
-            if(!isdigit(v[j][i]))
+            if(i < v[j].length() && !isdigit(v[j][i]))
                 cout << v[j][i];
         }
     }
