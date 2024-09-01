206011630bf0319ccb1d036e39f622ff1be1c2a3
Piau <piau7849@gmail.com>
2024-09-01 22:04:55 +0800

    0901

--- APCSS Summer Camp/D32.cpp
+++ APCSS Summer Camp/D32.cpp
@@ -4,13 +4,13 @@ using namespace std;
 int main() {
     int n, r;
     cin >> n >> r;
-    vector<bool> v(n);
-    fill(v.begin(), v.begin() + r, true);
+    vector<bool> v(n, 0);
+    for (int i = 0; i < r; i++) v[i] = 1;
 
     do {
         bool f = false;
-        for (int i = 1; i <= n; i++)
-            if (v[i-1]) { cout << (f ? " " : "") << i; f = true; }
+        for (int i = 0; i < n; i++)
+            if (v[i]) { cout << (f ? " " : "") << i+1; f = true; }
         cout << "\n";
     } while (prev_permutation(v.begin(), v.end()));
 }
